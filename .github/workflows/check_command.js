04094ca1be8e400169c2146eae49e4c51977ef24
Stuart Leeks <stuart@leeks.net>
2022-04-22 10:26:31 +0100

    overwrite to sync with main script

--- .github/workflows/check_command.js
+++ .github/workflows/check_command.js
@@ -1,171 +1,198 @@
 const { createHash } = require('crypto');
 
-async function getCommand({ context, core, github }) {
-	console.log(context);
-	console.log("==========================================================================================");
-	console.log(context.payload);
-	console.log("==========================================================================================");
+async function getCommandFromComment({ core, context, github }) {
+  const commentUsername = context.payload.comment.user.login;
+  const repoFullName = context.payload.repository.full_name;
+  const repoParts = repoFullName.split("/");
+  const repoOwner = repoParts[0];
+  const repoName = repoParts[1];
+  const prNumber = context.payload.issue.number;
+  const commentLink = context.payload.comment.html_url;
+  const runId = context.runId;
 
-	const commentUsername = context.payload.comment.user.login;
-	const repoFullName = context.payload.repository.full_name;
-	const repoParts = repoFullName.split("/");
-	const repoOwner = repoParts[0];
-	const repoName = repoParts[1];
+  // only allow actions for users with write access
+  if (!await userHasWriteAccessToRepo({ core, github }, commentUsername, repoOwner, repoName)) {
+    core.notice("Command: none - user doesn't have write permission]");
+    await github.rest.issues.createComment({
+      owner: repoOwner,
+      repo: repoName,
+      issue_number: prNumber,
+      body: `Sorry, @${commentUsername}, only users with write access to the repo can run pr-bot commands.`
+    });
+    return "none";
+  }
 
-	// only allow actions for users with write access
-	if (!await userHasWriteAccessToRepo({ github }, commentUsername, repoOwner, repoName)) {
-		console.log("Command: none [user doesn't have write permission]");
-		return "none";
-	}
+  // Determine PR SHA etc
+  const ciGitRef = getRefForPr(prNumber);
+  logAndSetOutput(core, "ciGitRef", ciGitRef);
 
-	core.debug('Test debug');
-	core.info('Test info');
-	core.warning('Test warning');
-	core.notice('Test notice');
+  const prRefId = getRefIdForPr(prNumber);
+  logAndSetOutput(core, "prRefId", prRefId);
 
-	const commentLink = context.payload.comment.html_url;
-	const commentBody = context.payload.comment.body;
-	const commentFirstLine = commentBody.split("\n")[0];
+  const pr = (await github.rest.pulls.get({ owner: repoOwner, repo: repoName, pull_number: prNumber })).data;
 
-	core.setOutput("testOutput1", false);
-	core.setOutput("testOutput2", "Hello!");
+  if (repoFullName === pr.head.repo.full_name) {
+    core.info(`Using head ref: ${pr.head.ref}`)
+    const branchRefId = getRefIdForBranch(pr.head.ref);
+    logAndSetOutput(core, "branchRefId", branchRefId);
+  } else {
+    core.info("Skipping branchRefId as PR is from a fork")
+  }
 
-	const prNumber = context.payload.issue.number;
-	const pr = (await github.rest.pulls.get({ owner: repoOwner, repo: repoName, pull_number: prNumber })).data;
-	console.log("==========================================================================================");
-	console.log(pr);
-	console.log("==========================================================================================");
+  const potentialMergeCommit = pr.merge_commit_sha;
+  logAndSetOutput(core, "prRef", potentialMergeCommit);
 
-	const prRefId = getRefIdForPr(prNumber);
-	console.log(`prRefId: ${prRefId}`);
-	core.setOutput("prRefId", prRefId);
-
-	console.log(`Using head ref: ${pr.head.ref}`)
-	const branchRefId = getRefIdForBranch(pr.head.ref);
-	console.log(`branchRefId: ${branchRefId}`);
-
-	const potentialMergeCommit = pr.merge_commit_sha;
-	console.log(`potentialMergeCommit: ${potentialMergeCommit}`);
-	core.setOutput("potentialMergeCommit", potentialMergeCommit);
-
-	const prHeadSha = pr.head.sha;;
-	console.log(`prHeadSha: ${prHeadSha}`);
-
-	github.rest.issues.createComment({
-		owner: repoOwner,
-		repo: repoName,
-		issue_number: prNumber,
-		body: `:robot: Running pr-bot in response to [the comment](${commentLink}) by @${commentUsername}\nprHeadSha:\`${prHeadSha}\`\n(Runid: ${context.runId})`
-	});
+  const prHeadSha = pr.head.sha;
+  logAndSetOutput(core, "prHeadSha", prHeadSha);
 
 
-	const prFilesResponse = await github.paginate(github.rest.pulls.listFiles, {
-		owner: repoOwner,
-		repo: repoName,
-		pull_number: prNumber
-	});
-	const prFiles = prFilesResponse.map(file => file.filename);
-	console.log("==========================================================================================");
-	console.log(prFiles);
-	console.log("==========================================================================================");
+  //
+  // Determine what action to take
+  // Only use the first line of the comment to allow remainder of the body for other comments/notes
+  //
+  const commentBody = context.payload.comment.body;
+  const commentFirstLine = commentBody.split("\n")[0];
+  let command = "none";
+  const trimmedFirstLine = commentFirstLine.trim();
+  if (trimmedFirstLine[0] === "/") {
+    switch (trimmedFirstLine) {
+      case "/test":
+        command = "run-tests";
+        const runTestMessage = `Running tests: `;
+        await addActionComment({ github }, repoOwner, repoName, prNumber, commentUsername, commentLink, runTestMessage);
+        break;
 
-	const docsRegexes = [/README\.md/, /\.md$/, /\.txt$/];
-	const gotNonDocChanges = prFiles.some(file => docsRegexes.every(regex => !regex.test(file)));
-	console.log(`gotNonDocChanges: ${gotNonDocChanges}`);
-	console.log("==========================================================================================");
+      case "/test-extended":
+        command = "run-tests-extended";
+        break;
 
-	const trimmedFirstLine = commentFirstLine.trim();
-	var command = "none"
-	if (trimmedFirstLine[0] === '/') {
-		switch (trimmedFirstLine) {
-			case "/test":
-				console.log("Result: run-tests");
-				command = "run-tests";
-				break;
-			case "/help":
-				console.log("Result: show-help");
-				await showHelp(github, repoOwner, repoName, prNumber);
-				command = "none"; // command already handled :-)
-				break;
-			default:
-				console.log("not recognised as a valid command");
-				await showHelp(github, repoOwner, repoName, prNumber, trimmedFirstLine);
-				command = "none";
-				break;
-		}
-	} else {
-		console.log("not a command")
-		command = "none";
-	}
-	console.log(`command: ${command}`);
-	core.setOutput("command", command);
-	return command;
+      case "/test-force-approve":
+        command = "test-force-approve";
+        break;
+
+      case "/test-destroy-env":
+        command = "test-destroy-env";
+        break;
+
+      case "/help":
+        showHelp({ github }, repoOwner, repoName, prNumber, null);
+        command = "none"; // command has been handled, so don't need to return a value for future steps
+        break;
+
+      default:
+        core.warning(`'${trimmedFirstLine}' not recognised as a valid command`);
+        await showHelp({github}, repoOwner, repoName, prNumber, trimmedFirstLine);
+        command = "none";
+        break;
+    }
+  }
+  logAndSetOutput(core, "command", command);
+  return command;
 }
 
-async function showHelp(github, repoOwner, repoName, prNumber, invalidCommand) {
-	const leadingContent = invalidCommand ? `\`${invalidCommand}\` is not recognised as a valid command.` : "Hello!";
+async function labelAsExternalIfAuthorDoesNotHaveWriteAccess({ core, context, github }) {
+  const username = context.payload.pull_request.user.login;
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
 
-	const body = `${leadingContent}
+  if (!await userHasWriteAccessToRepo({ core, github }, username, owner, repo)) {
+    core.info("Adding external label to PR " + context.payload.pull_request.number)
+    await github.rest.issues.addLabels({
+      owner,
+      repo,
+      issue_number: context.payload.pull_request.number,
+      labels: ['external']
+    });
+  }
+}
+
+async function userHasWriteAccessToRepo({ core, github }, username, repoOwner, repoName) {
+  // Previously, we attempted to use github.event.comment.author_association to check for OWNER or COLLABORATOR
+  // Unfortunately, that always shows MEMBER if you are in the microsoft org and have that set to publicly visible
+  // (Can check via https://github.com/orgs/microsoft/people?query=<username>)
+
+  // https://docs.github.com/en/rest/reference/collaborators#check-if-a-user-is-a-repository-collaborator
+  let userHasWriteAccess = false;
+  try {
+    core.info(`Checking if user "${username}" has write access to ${repoOwner}/${repoName} ...`)
+    const result = await github.request('GET /repos/{owner}/{repo}/collaborators/{username}', {
+      owner: repoOwner,
+      repo: repoName,
+      username
+    });
+    userHasWriteAccess = result.status === 204;
+  } catch (err) {
+    if (err.status === 404) {
+      core.info("User not found in collaborators");
+    } else {
+      core.error(`Error checking if user has write access: ${err.status} (${err.response.data.message}) `)
+    }
+  }
+  core.info("User has write access: " + userHasWriteAccess);
+  return userHasWriteAccess
+}
+
+async function showHelp({ github }, repoOwner, repoName, prNumber, invalidCommand) {
+  const leadingContent = invalidCommand ? `\`${invalidCommand}\` is not recognised as a valid command.` : "Hello!";
+
+  const body = `${leadingContent}
 
 You can use the following commands:
+&nbsp;&nbsp;&nbsp;&nbsp;/test - build, deploy and run smoke tests on a PR
+&nbsp;&nbsp;&nbsp;&nbsp;/test-extended - build, deploy and run smoke & extended tests on a PR
+&nbsp;&nbsp;&nbsp;&nbsp;/test-force-approve - force approval of the PR tests (i.e. skip the deployment checks)
+&nbsp;&nbsp;&nbsp;&nbsp;/test-destroy-env - delete the validation environment for a PR (e.g. to enable testing a deployment from a clean start after previous tests)
+&nbsp;&nbsp;&nbsp;&nbsp;/help - show this help`;
 
-|command|description|
-|-|-|
-|\`/test\`               | build, deploy and run smoke tests on a PR|
-|\`/test-extended\`      | build, deploy and run smoke & extended tests on a PR|
-|\`/test-force-approve\` | force approval of the PR tests (i.e. skip the deployment checks)|
-|\`/test-destroy-env\`   | delete the validation environment for a PR (e.g. to enable testing a deployment from a clean start after previous tests)|
-|\`/help\`               | show this help|`;
+  await github.rest.issues.createComment({
+    owner: repoOwner,
+    repo: repoName,
+    issue_number: prNumber,
+    body: body
+  });
 
-	await github.rest.issues.createComment({
-		owner: repoOwner,
-		repo: repoName,
-		issue_number: prNumber,
-		body: body,
-	});
+}
+async function addActionComment({ github }, repoOwner, repoName, prNumber, commentUser, commentLink, message) {
+
+  const body = `:robot: pr-bot
+
+${message}
+
+(in response to [this comment](${commentLink}) from @${commentUser})
+`;
+
+  await github.rest.issues.createComment({
+    owner: repoOwner,
+    repo: repoName,
+    issue_number: prNumber,
+    body: body
+  });
 
 }
 
-
-async function userHasWriteAccessToRepo({ github }, username, repoOwner, repoName) {
-	// Previously, we attempted to use github.event.comment.author_association to check for OWNER or COLLABORATOR
-	// Unfortunately, that always shows MEMBER if you are in the microsoft org and have that set to publicly visible
-	// (Can check via https://github.com/orgs/microsoft/people?query=<username>)
-
-	// https://docs.github.com/en/rest/reference/collaborators#check-if-a-user-is-a-repository-collaborator
-	let userHasWriteAccess = false;
-	try {
-		console.log(`Checking if user "${username}" has write access to ${repoOwner}/${repoName} ...`)
-		const result = await github.request('GET /repos/{owner}/{repo}/collaborators/{username}', {
-			owner: repoOwner,
-			repo: repoName,
-			username
-		});
-		userHasWriteAccess = result.status === 204;
-	} catch (err) {
-		if (err.status === 404) {
-			console.log("User not found in collaborators");
-		} else {
-			console.log(`Error checking if user has write access: ${err.status} (${err.response.data.message}) `)
-		}
-	}
-	console.log("User has write access: " + userHasWriteAccess);
-	return userHasWriteAccess
+function logAndSetOutput(core, name, value) {
+  core.info(`Setting output '${name}: ${value}`);
+  core.setOutput(name, value);
 }
 
+function getRefForPr(prNumber) {
+  return `refs/pull/${prNumber}/merge`;
+}
 function getRefIdForPr(prNumber) {
-	// Determine newline is for compatibility with previous bash SHA calculation
-	return createShortHash(`refs/pull/${prNumber}/merge\n`);
+  const prRef = getRefForPr(prNumber);
+  // Trailing newline is for compatibility with previous bash SHA calculation
+  return createShortHash(`${prRef}\n`);
 }
 function getRefIdForBranch(branchName) {
-	// Determine newline is for compatibility with previous bash SHA calculation
-	return createShortHash(`refs/heads/${branchName}\n`);
+  // Trailing newline is for compatibility with previous bash SHA calculation
+  return createShortHash(`refs/heads/${branchName}\n`);
 }
 function createShortHash(ref) {
-	const hash = createHash('sha1').update(ref, 'utf8').digest('hex')
-	return hash.substring(0, 8);
+  const hash = createHash('sha1').update(ref, 'utf8').digest('hex')
+  return hash.substring(0, 8);
 }
 
 module.exports = {
-	getCommand
+  getCommandFromComment,
+  labelAsExternalIfAuthorDoesNotHaveWriteAccess
 }
